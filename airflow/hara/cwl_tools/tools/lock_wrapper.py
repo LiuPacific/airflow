cc4b274f5cd43b5fd3f97b581c23fbf5a954b490
LiuPacific <28793803@qq.com>
2024-09-06 19:37:13 -0500

    gate keeper lock

---
+++ airflow/hara/cwl_tools/tools/lock_wrapper.py
@@ -0,0 +1,25 @@
+import fcntl
+import time
+import os
+from functools import wraps
+
+def resource_lock_decorator(lock_file_path):
+    def ha_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            lock_file = open(lock_file_path, 'w')
+            try:
+                fcntl.flock(lock_file, fcntl.LOCK_EX)
+                print(f'lock acquired for {func.__name__}')
+                result = func(*args, **kwargs)
+                return result
+            finally:
+                fcntl.flock(lock_file, fcntl.LOCK_UN)
+                lock_file.close()
+                print(f'lock released for {func.__name__}')
+        return wrapper
+    return ha_decorator
+
+
+
+
---
+++ airflow/hara/cwl_tools/tools/test_lock_wrapper.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+from airflow.hara.cwl_tools.tools.lock_wrapper import resource_lock_decorator
+import time
+
+
+
+class TestLockWrapper(unittest.TestCase):
+    def setUp(self):
+        print('hara test wrapper start')
+
+    @resource_lock_decorator("./ha.lock")
+    def process_files(self, file_path):
+        if os.path.exists(file_path):
+            with open(file_path, 'r+') as f:
+                data = f.read()
+                # Simulate some processing (e.g., modifying the content)
+                time.sleep(5)
+                print(f"Processing {file_path}: {data}")
+        else:
+            print(f"File {file_path} does not exist.")
+
+    def test1(self):
+        self.process_files('./ha.lock')
+
+    def test2(self):
+        self.process_files('./ha.lock')
